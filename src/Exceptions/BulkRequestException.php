3210d7ac15dbb361cdba5121622528d95228475a
Steve Bauman <steven_bauman@outlook.com>
2022-01-06 12:54:38 -0500

    Add first error to exception message for clearer debugging
    Closes #21

--- src/Exceptions/BulkRequestException.php
+++ src/Exceptions/BulkRequestException.php
@@ -13,16 +13,40 @@ final class BulkRequestException extends ErrorException
 
     public function __construct(array $response)
     {
-        parent::__construct(
-            'One or more bulk operations did not complete successfully. ' .
-            'Catch the exception and use the BulkRequestException::getResponse() method to get more details.'
-        );
-
         $this->response = $response;
+
+        parent::__construct(
+            ($this->getFirstErrorFromResponse() ?? 'One or more bulk operations did not complete successfully.') .
+            ' Catch the exception and use the BulkRequestException::getResponse() method to get more details.'
+        );
     }
 
     public function getResponse(): array
     {
         return $this->response;
+    }
+
+    public function getResponseItems(): array
+    {
+        return $this->response['items'] ?? [];
+    }
+
+    private function getFirstErrorFromResponse(): ?string
+    {
+        $items = $this->getResponseItems();
+
+        foreach ($items as $item) {
+            foreach ($item ?? [] as $response) {
+                $count = count($items);
+                $type = $response['error']['type'] ?? 'NULL';
+                $reason = $response['error']['reason'] ?? 'NULL';
+
+                return "$count bulk operation(s) did not complete successfully. " .
+                    ($count > 1 ? "First error: " : "Error: ") .
+                    "$type. Reason: $reason.";
+            }
+        }
+
+        return null;
     }
 }
